0c1bb5eb8af6b4c95d7b731f4c95ccdbfded7d0e
Ser Rocal <srodriguezcalvo@uoc.edu>
2025-04-08 17:51:17 +0200

    Comentarios en propiedades

--- pra1.js
+++ pra1.js
@@ -1,14 +1,14 @@
 class Film {
     constructor(id, title, overview, popularity, poster_path, release_date, vote_average, vote_count, genre_ids) {
-        this.id = id;
-        this.title = title;
-        this.overview = overview;
-        this.popularity = popularity;
-        this.poster_path = poster_path;
-        this.release_date = release_date;
-        this.vote_average = vote_average;
-        this.vote_count = vote_count;
-        this.genre_ids = genre_ids;
+        this.id = id; //identificador único
+        this.title = title; //título de la película
+        this.overview = overview; //Resumen de la película
+        this.popularity = popularity; //Popularidad
+        this.poster_path = poster_path; //Poster de la película
+        this.release_date = release_date; //Fecha de lanzamiento
+        this.vote_average = vote_average; //Promedio de votos
+        this.vote_count = vote_count; //Número de votos
+        this.genre_ids = genre_ids; //Lista de géneros por los que se ha clasificado la película
     }
 
     //Getters y setters
