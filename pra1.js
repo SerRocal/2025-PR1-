c60d69c562eb906b35184898296cc75b411b61ae
SerRocal <srodriguezcalvo@uoc.edu>
2025-04-09 11:20:50 +0200

    Getters y Setters de la Id y Título, restringiendo su valor para una mejor práctica en el código y un programa más optimizado

--- pra1.js
+++ pra1.js
@@ -1,18 +1,42 @@
 class Film {
     constructor(id, title, overview, popularity, poster_path, release_date, vote_average, vote_count, genre_ids) {
-        this.id = id; //identificador único
-        this.title = title; //título de la película
-        this.overview = overview; //Resumen de la película
-        this.popularity = popularity; //Popularidad
-        this.poster_path = poster_path; //Poster de la película
-        this.release_date = release_date; //Fecha de lanzamiento
-        this.vote_average = vote_average; //Promedio de votos
-        this.vote_count = vote_count; //Número de votos
-        this.genre_ids = genre_ids; //Lista de géneros por los que se ha clasificado la película
+        this._id = id; //identificador único
+        this._title = title; //título de la película
+        this._overview = overview; //Resumen de la película
+        this._popularity = popularity; //Popularidad
+        this._poster_path = poster_path; //Poster de la película
+        this._release_date = release_date; //Fecha de lanzamiento
+        this._vote_average = vote_average; //Promedio de votos
+        this._vote_count = vote_count; //Número de votos
+        this._genre_ids = genre_ids; //Lista de géneros por los que se ha clasificado la película
     }
 
-    //Getters y setters
+    //getters y setters
 
+    get id() {
+        return this._id;
+    }
+
+    set id(newId) {
+        if (typeof newId === 'number' && newId >= 0) {
+            this._id = newId;
+        } else {
+            console.error("El título debe ser un número positivo.");
+        }
+
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(newTitle) {
+        if (typeof newTitle === 'string') {
+            this._title = newTitle;
+        } else {
+            console.error("El ID debe ser un nombre")
+        }
+    }
 }
 
 class FilmList {
