05d2d8035105985656385aa89025d5b86b237ab4
Ser Rocal <srodriguezcalvo@uoc.edu>
2025-04-09 15:04:20 +0200

    Completar clase Film con campos privados y acceso controlado
    - Declarar explícitamente los campos privados (#) de la clase.
    - Añadir getters y setters públicos para todas las propiedades.
    - Implementar validaciones básicas en los setters (números, formato fecha, strings, array).

--- pra1.js
+++ pra1.js
@@ -1,42 +1,96 @@
 class Film {
+    //declaración de los campos privados al principio de la clase como recomendación (si no VS subraya las propiedades como dando un warning)
+    #id;
+    #title;
+    #overview;
+    #popularity;
+    #poster_path;
+    #release_date;
+    #vote_average;
+    #vote_count;
+    #genre_ids;
+
     constructor(id, title, overview, popularity, poster_path, release_date, vote_average, vote_count, genre_ids) {
-        this._id = id; //identificador único
-        this._title = title; //título de la película
-        this._overview = overview; //Resumen de la película
-        this._popularity = popularity; //Popularidad
-        this._poster_path = poster_path; //Poster de la película
-        this._release_date = release_date; //Fecha de lanzamiento
-        this._vote_average = vote_average; //Promedio de votos
-        this._vote_count = vote_count; //Número de votos
-        this._genre_ids = genre_ids; //Lista de géneros por los que se ha clasificado la película
+        this.#id = id; //identificador único
+        this.#title = title; //título de la película
+        this.#overview = overview; //Resumen de la película
+        this.#popularity = popularity; //Popularidad
+        this.#poster_path = poster_path; //Poster de la película
+        this.#release_date = release_date; //Fecha de lanzamiento
+        this.#vote_average = vote_average; //Promedio de votos
+        this.#vote_count = vote_count; //Número de votos
+        this.#genre_ids = genre_ids; //Lista de géneros por los que se ha clasificado la película
     }
 
     //getters y setters
 
     get id() {
-        return this._id;
+        return this.#id;
     }
-
     set id(newId) {
         if (typeof newId === 'number' && newId >= 0) {
-            this._id = newId;
+            this.#id = newId;
         } else {
-            console.error("El título debe ser un número positivo.");
+            console.error("El ID es inválido:", newId);
         }
-
     }
 
     get title() {
-        return this._title;
+        return this.#title;
     }
-
     set title(newTitle) {
         if (typeof newTitle === 'string') {
-            this._title = newTitle;
+            this.#title = newTitle;
         } else {
-            console.error("El ID debe ser un nombre")
+            console.error("El título es inválido:", newTitle);
         }
     }
+
+    get overview() {
+        return this.#overview;
+    }
+
+    set overview(newOverview) {
+        if (typeof newOverview === 'string') {
+            this.#overview = newOverview;
+        } else {
+            console.error("El Overview es inválido:", newOverview);
+        }
+    }
+
+    get popularity() {
+        return this.#popularity;
+    }
+
+    set popularity(newPopularity) {
+        if (typeof newPopularity === 'number' && newPopularity >= 0) {
+            this.#popularity = newPopularity;
+        } else {
+            console.error("La Popularidad es inválida:", newPopularity);
+        }
+    }
+
+    get poster_path() {
+        return this.#poster_path;
+    }
+
+    get release_date() {
+        return this.#release_date;
+    }
+
+    set release_date(newReleaseDate) {
+        const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
+        if (typeof newReleaseDate === 'string' && dateFormatRegex.test(newReleaseDate)) {
+            this.#release_date = newReleaseDate;
+        } else {
+            console.error("Release date inválida o formato no es YYYY-MM-DD:", newReleaseDate);
+        }
+    }
+
+    get vote_average() {
+        return this.#vote_average;
+    }
+
 }
 
 class FilmList {
